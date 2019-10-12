64218cdb0757a5bc6c7cc66b3646dece83c54c01
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-12 04:03:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,11 +9,12 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number, sleep=1)
+def countdown_with_sleep(number, sleep)
   number = 10 
     while number > 0 
     puts "#{number} SECOND(S)!"
     number -= 1 
+    sleep(1)
   end 
 "HAPPY NEW YEAR!"
 end 
